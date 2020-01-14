0b64e4dfea4871aae2f11a6831708cc1a3c25201
Joshua Hügli <joshua@trafficteam.ch>
2020-01-14 16:13:29 +0100

    Revert "Use comprehension for clarity and best practice"
    This reverts commit 90ad0fa05112d86555e9e4e65de7467b05ef12c0.

--- octodns/provider/ovh.py
+++ octodns/provider/ovh.py
@@ -323,7 +323,7 @@ class OvhProvider(BaseProvider):
                           'n': lambda _: True,
                           'g': lambda _: True}
 
-        splitted = [v for v in value.split('\\;') if v]
+        splitted = list(filter(None, value.split('\\;')))
         found_key = False
         for splitted_value in splitted:
             sub_split = [x.strip() for x in splitted_value.split("=", 1)]
